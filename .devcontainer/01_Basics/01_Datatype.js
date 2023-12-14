2f9c885d521ca35497c897e03c744997791891f8
sharjati <153422432+sharjati@users.noreply.github.com>
2023-12-14 16:47:15 +0000

    Datatyoes

---
+++ .devcontainer/01_Basics/01_Datatype.js
@@ -0,0 +1,25 @@
+"use strict";  // treat all js code as newer version
+
+console.log(3 + 3);
+console.log("Hitesh")
+
+let name = "Histesh"
+let age = 18
+let isLoggedIn = false
+
+
+// number = 2to power 53
+// bigint 
+// string =>""
+// boolean = true/false
+// null 
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);
+ //aadajj
+
+
